1db86e9e2f3bc5d7c45c4a815444e4f884f60622
Chris Camfield <ccamfield@sagomini.com>
2020-03-04 11:34:03 -0500

    New version of DoSomething.

--- DoSomething.cs
+++ DoSomething.cs
@@ -7,6 +7,6 @@ public class DoSomething : MonoBehaviour
     
     public void OnButtonPress()
     {
-        Debug.Log("The Quick Brown Fox Jumped Over The Lazy Dog");
+        Debug.Log("This is the third revision to DoSomething");
     }
 }
