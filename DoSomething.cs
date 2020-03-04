d2cae100380737a9a189de1142523122b8aa8b51
Chris Camfield <ccamfield@sagomini.com>
2020-03-04 11:19:16 -0500

    Added basic version of DoSomething

---
+++ DoSomething.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoSomething : MonoBehaviour
+{
+    
+    public void OnButtonPress()
+    {
+        Debug.Log("Button has been pressed");
+    }
+}
